3b532378054018de1381e988c926620a6142bc95
Eli <eli.lennox@cocdecov.io>
2025-09-03 20:32:24 -0400

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: process.env.PUBLIC_SITE_URL || 'http://localhost:4321',
-  base: process.env.PUBLIC_SITE_URL ? new URL(process.env.PUBLIC_SITE_URL).pathname : '/',
+  base: '/',
   integrations: [tailwind(), react()],
   devToolbar: {
     enabled: false
